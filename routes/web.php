b795382388f9fe5f380ed35182e68c816b231f5a
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-02-05 13:39:32 +0800

    add validation

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,6 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('home');
-<<<<<<< HEAD
-});
-
-Route::post('/register', function () {
-    return 'thank you';
-=======
->>>>>>> f9485d1cda22cc8aac8e08cb4ff57c149a0d8bb3
 });
 
 Route::post('/register', [UserController::class, 'register']);
