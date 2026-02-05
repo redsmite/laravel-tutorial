a559cd0da7aabea622b03f31ca37ec5d1776a1ae
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-02-05 13:11:44 +0800

    create a simple login page (no database connection)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <div style="border: 3px solid black">
+    <h2>Register</h2>
+    <form action="/register" method="POST">
+        @csrf
+        <input type="text" name="name" placeholder="Name">
+        <input type="email" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Password">
+        <button>Register</button>
+    </form>
+  </div>
+</body>
+</html>
