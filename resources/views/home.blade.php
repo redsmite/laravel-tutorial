70dfba7a11456c49d6ec5e63ef5562978900d9df
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-02-05 18:17:25 +0800

    edit post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,6 +30,12 @@
       <div style="background-color: gray; padding: 10px; margin: 10px;">
       <h3>{{$post['title']}}</h3>
       {{$post['body']}}
+        <p><a href="/edit-post/{{$post->id}}">Edit</a></p>
+        <form action="/delete-post/{{$post->id}}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button>Delete</button>
+        </form>
       </div>
       @endforeach
     </div>
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,14 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    $posts = Post::all();
+    $posts = [];
+    if(auth()->check()){
+            $posts = auth()->user()->usersCoolPost()->latest()->get();
+
+    }
+
     return view('home', ['posts' => $posts]);
+    
 });
 
 Route::post('/register', [UserController::class, 'register']);
@@ -19,3 +25,5 @@ Route::post('/login', [UserController::class, 'login']);
 // Blog post related routes
 
 Route::post('/create-post', [PostController::class, 'createPost']);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
